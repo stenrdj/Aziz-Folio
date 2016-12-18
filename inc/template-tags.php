5c16c93bb8bebf7f57c4d9d9d2cf15b7c32f3583
Aziz Segaoui <Aziz@Azizs-MacBook-Pro-2.local>
2016-12-18 09:57:14 +0000

    Adding some tricks
    Making the primary menu selected when visiting a custom post type
    taxonomy or single page

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -190,3 +190,31 @@ function custom_navigation_menus() {
 add_action( 'init', 'custom_navigation_menus' );
 
 
+// Mark the post type as selected whene a taxonomy or singel page visited. 
+// Exemple : visite -> cats/3 -> projects in meny will be marked and selected
+
+  add_action('nav_menu_css_class', 'add_current_nav_class', 10, 2 );
+	
+	function add_current_nav_class($classes, $item) {
+		
+		// Getting the current post details
+		global $post;
+		
+		// Getting the post type of the current post
+		$current_post_type = get_post_type_object(get_post_type($post->ID));
+		$current_post_type_slug = $current_post_type->rewrite[slug];
+			
+		// Getting the URL of the menu item
+		$menu_slug = strtolower(trim($item->url));
+		
+		// If the menu item URL contains the current post types slug add the current-menu-item class
+		if (strpos($menu_slug,$current_post_type_slug) !== false) {
+		
+		   $classes[] = 'current-menu-item';
+		
+		}
+		
+		// Return the corrected set of classes to be added to the menu item
+		return $classes;
+	
+}
